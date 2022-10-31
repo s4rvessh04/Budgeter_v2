70649bcc585d5f436c155f7bba1b2bde72b705ab
sarveshrane2000 <sarveshrane2000@gmail.com>
2022-10-31 22:45:59 +0530

    feat: added default error page

---
+++ frontend/src/pages/Error.tsx
@@ -0,0 +1,53 @@
+import {
+	Center,
+	Container,
+	Divider,
+	Spacer,
+	Stack,
+	StackDivider,
+	Text,
+	useColorModeValue,
+} from "@chakra-ui/react";
+
+export const Error = () => {
+	return (
+		<Container
+			maxW="container.xl"
+			h={"100vh"}
+			minW={"100vw"}
+			bg={useColorModeValue("white", "black")}
+		>
+			<Center maxH={"100vh"} h="full">
+				<Stack
+					direction={["column", "row"]}
+					spacing="2"
+					divider={
+						<StackDivider
+							borderColor={useColorModeValue(
+								"gray.300",
+								"gray.800"
+							)}
+						/>
+					}
+				>
+					<Text
+						fontFamily={"mono"}
+						fontSize={"xl"}
+						alignSelf={"center"}
+						textColor={useColorModeValue("gray.400", "gray.700")}
+					>
+						404
+					</Text>
+					<Text
+						fontSize={"2xl"}
+						fontWeight={"semibold"}
+						textColor={useColorModeValue("gray.900", "white")}
+						alignSelf={"center"}
+					>
+						Page does not exist!
+					</Text>
+				</Stack>
+			</Center>
+		</Container>
+	);
+};
