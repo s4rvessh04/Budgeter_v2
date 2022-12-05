f1860828132c3c8f079ef7eb745ff0638c018a90
sarveshrane2000 <sarveshrane2000@gmail.com>
2022-12-05 23:17:29 +0530

    feat: multiple shared users handling

--- frontend/src/components/ExpenseEditModal.tsx
+++ frontend/src/components/ExpenseEditModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import {
 	Button,
 	FormControl,
@@ -24,36 +24,57 @@ import {
 	useColorModeValue,
 	Box,
 	useToast,
-	IconButton,
 	Select,
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	Icon,
+	Grid,
+	GridItem,
 } from "@chakra-ui/react";
-import { FiUser, FiUsers, FiX } from "react-icons/fi";
+import { FiChevronDown, FiUser, FiUsers } from "react-icons/fi";
 import { parseAmount, parseDate } from "../utils";
-
-type displayData = {
-	dateTime: string;
-	description: string;
-	amount: string;
-	isShared: boolean;
-};
+import { IExpense, ISharedExpense } from "../types";
 
 interface Props {
 	onClose: () => void;
 	isOpen: boolean;
-	displayData: displayData;
+	data: IExpense | undefined;
 }
 
-export const ExpenseEditModal = ({ onClose, isOpen, displayData }: Props) => {
-	const parse = (val: string) => val.replace(/^\$/, "");
-
+export const ExpenseEditModal = ({ onClose, isOpen, data }: Props) => {
 	const toast = useToast();
-	const [value, setValue] = React.useState(displayData?.amount);
+	const dateTime = parseDate(data!?.dateTime);
 
-	const dateTime = parseDate(displayData!?.dateTime);
+	const [sharedExpenses, setSharedExpenses] = React.useState<
+		ISharedExpense[]
+	>(data!?.sharedExpenses);
 
-	useEffect(() => {
-		setValue(displayData?.amount);
-	}, [displayData]);
+	React.useEffect(() => {
+		setSharedExpenses(data!?.sharedExpenses);
+		return;
+	}, [data]);
+
+	function handleSharedExpUserCount(type: "inc" | "dec"): void {
+		if (type === "inc") {
+			setSharedExpenses([
+				...sharedExpenses,
+				{
+					id: "0",
+					expenseId: "0",
+					lastUpdateTime: "0",
+					mainUserId: "0",
+					sharedUserAmount: 0,
+					sharedUserId: "0",
+					status: "unpaid",
+				},
+			]);
+		} else {
+			sharedExpenses.pop();
+			setSharedExpenses([...sharedExpenses]);
+		}
+	}
 
 	return (
 		<>
@@ -91,34 +112,29 @@ export const ExpenseEditModal = ({ onClose, isOpen, displayData }: Props) => {
 									colorScheme="telegram"
 								>
 									<TagLeftIcon boxSize="12px">
-										{displayData?.isShared ? (
+										{data?.isShared ? (
 											<FiUsers size={"24px"} />
 										) : (
 											<FiUser size={"24px"} />
 										)}
 									</TagLeftIcon>
 									<TagLabel>
-										{displayData?.isShared
-											? "Shared"
-											: "Self"}
+										{data?.isShared ? "Shared" : "Self"}
 									</TagLabel>
 								</Tag>
 							</Box>
 						</Flex>
-
 						<FormControl mt={4}>
 							<FormLabel>Description</FormLabel>
-							<Input placeholder={displayData?.description} />
+							<Input
+								defaultValue={data?.description}
+								placeholder="Enter description"
+								required
+							/>
 						</FormControl>
 						<FormControl mt={4}>
 							<FormLabel>Amount</FormLabel>
-							<NumberInput
-								onChange={(valueString) =>
-									setValue(parse(valueString))
-								}
-								value={parseAmount(value)}
-								step={100}
-							>
+							<NumberInput value={data?.amount} step={100}>
 								<NumberInputField />
 								<NumberInputStepper>
 									<NumberIncrementStepper />
@@ -126,27 +142,43 @@ export const ExpenseEditModal = ({ onClose, isOpen, displayData }: Props) => {
 								</NumberInputStepper>
 							</NumberInput>
 						</FormControl>
-						{displayData?.isShared ? (
+						{data?.isShared && sharedExpenses?.length !== 0 ? (
 							<FormControl isRequired mt={4}>
 								<FormLabel>With</FormLabel>
-								{Array(3)
-									.fill(0)
-									.map((key, i) => (
-										<Flex gap="4" mb="4">
-											<Select>
-												<option value="option1">
-													Name 1
-												</option>
-												<option value="option2">
-													Name 2
-												</option>
-												<option value="option3">
-													Name 3
+								{sharedExpenses?.map((sharedExpense, idx) => (
+									<Grid
+										gap="4"
+										mb="4"
+										key={idx}
+										templateColumns={"repeat(6, 1fr)"}
+									>
+										<GridItem
+											colSpan={3}
+											display="flex"
+											alignItems="center"
+											justifyContent={"start"}
+										>
+											<Select
+												defaultValue={
+													sharedExpense.sharedUserId
+												}
+											>
+												<option>
+													{sharedExpense.sharedUserId}
 												</option>
 											</Select>
+										</GridItem>
+										<GridItem
+											colSpan={2}
+											display="flex"
+											alignItems="center"
+											justifyContent={"start"}
+										>
 											<NumberInput
 												step={100}
-												defaultValue={0}
+												defaultValue={
+													sharedExpense.sharedUserAmount
+												}
 												min={0}
 											>
 												<NumberInputField />
@@ -155,26 +187,112 @@ export const ExpenseEditModal = ({ onClose, isOpen, displayData }: Props) => {
 													<NumberDecrementStepper />
 												</NumberInputStepper>
 											</NumberInput>
-											<IconButton
-												aria-label="remove-btn"
-												colorScheme={"gray"}
-												icon={<FiX />}
-												// onClick={() =>
-												// 	handleSharedExpenseUserCount(
-												// 		"dec"
-												// 	)
-												// }
-											/>
-										</Flex>
-									))}
+										</GridItem>
+										<GridItem
+											colSpan={1}
+											display="flex"
+											alignItems="center"
+											justifyContent={"end"}
+										>
+											<Menu>
+												<MenuButton
+													p="2"
+													as={Button}
+													color={useColorModeValue(
+														"white",
+														"gray.700"
+													)}
+													_hover={{
+														bg:
+															sharedExpense?.status ===
+															"paid"
+																? useColorModeValue(
+																		"green.600",
+																		"green.300"
+																  )
+																: useColorModeValue(
+																		"red.600",
+																		"red.300"
+																  ),
+													}}
+													_active={{
+														bg:
+															sharedExpense?.status ===
+															"paid"
+																? useColorModeValue(
+																		"green.600",
+																		"green.300"
+																  )
+																: useColorModeValue(
+																		"red.600",
+																		"red.300"
+																  ),
+													}}
+													bg={
+														sharedExpense?.status ===
+														"paid"
+															? useColorModeValue(
+																	"green.500",
+																	"green.200"
+															  )
+															: useColorModeValue(
+																	"red.500",
+																	"red.200"
+															  )
+													}
+												>
+													<Icon
+														as={FiChevronDown}
+														size="4"
+														boxSize={"4"}
+														mt="1"
+													/>
+												</MenuButton>
+												<MenuList px="2">
+													<MenuItem
+														rounded={"md"}
+														border="none"
+														_hover={{
+															border: "none",
+														}}
+													>
+														Mark{" "}
+														{sharedExpense?.status ===
+														"paid"
+															? "Unpaid"
+															: "Paid"}
+													</MenuItem>
+													<MenuItem
+														color={useColorModeValue(
+															"red.600",
+															"red.300"
+														)}
+														rounded={"md"}
+														border="none"
+														_hover={{
+															border: "none",
+														}}
+														onClick={() =>
+															handleSharedExpUserCount(
+																"dec"
+															)
+														}
+													>
+														Remove
+													</MenuItem>
+												</MenuList>
+											</Menu>
+										</GridItem>
+									</Grid>
+								))}
 								<Flex>
 									<Button
 										w={"full"}
 										variant="ghost"
 										colorScheme={"blue"}
-										// onClick={() =>
-										// 	handleSharedExpenseUserCount("inc")
-										// }
+										onClick={() =>
+											handleSharedExpUserCount("inc")
+										}
 									>
 										Add new
 									</Button>
