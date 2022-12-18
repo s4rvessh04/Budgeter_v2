379a7f32a41fec719f7d7480f0786a2b522a20e4
sarveshrane2000 <sarveshrane2000@gmail.com>
2022-12-18 22:01:50 +0530

    Removed zotai files

--- frontend/src/atoms/Home.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from "jotai";
-
-export const balanceAmountAtom = atom<number>(0);
-export const expenseAmountAtom = atom<number>(0);
-export const avgSavingsAmountAtom = atom<number>(0);
-
-export const settleExpensesAmountAtom = atom<number>(0);
-export const oweExpensesAmountAtom = atom<number>(0);
