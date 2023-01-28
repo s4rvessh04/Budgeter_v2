23961599a91b46030f1886996dc5aa55ec27ff60
sarveshrane2000 <sarveshrane2000@gmail.com>
2023-01-28 20:13:16 +0530

    ref: black formatting

--- backend/friend/migrations/0001_initial.py
+++ backend/friend/migrations/0001_initial.py
@@ -7,16 +7,25 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Friend',
+            name="Friend",
             fields=[
-                ('id', models.IntegerField(primary_key=True, serialize=False)),
-                ('last_updated', models.DateTimeField()),
-                ('status', models.CharField(choices=[('A', 'Accepted'), ('R', 'Rejected'), ('P', 'Pending')], max_length=1)),
+                ("id", models.IntegerField(primary_key=True, serialize=False)),
+                ("last_updated", models.DateTimeField()),
+                (
+                    "status",
+                    models.CharField(
+                        choices=[
+                            ("A", "Accepted"),
+                            ("R", "Rejected"),
+                            ("P", "Pending"),
+                        ],
+                        max_length=1,
+                    ),
+                ),
             ],
         ),
     ]
