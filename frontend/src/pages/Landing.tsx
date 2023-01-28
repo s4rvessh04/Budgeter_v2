1dd11bddbc3aa789075cb970d34e1d7d94d5765f
sarveshrane2000 <sarveshrane2000@gmail.com>
2023-01-28 20:14:54 +0530

    add: landing page

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,44 +1,93 @@
-import { Button } from "@chakra-ui/react";
-import axios from "axios";
-import React from "react";
-import { useCookies } from "react-cookie";
-import { useQuery } from "react-query";
+import {
+	Box,
+	Button,
+	Flex,
+	Heading,
+	HStack,
+	Image,
+	Stack,
+	Text,
+} from "@chakra-ui/react";
 import { useLocation } from "wouter";
+import viteSvg from "../../public/vite.svg";
+import heroBackgroundImage from "../assets/hero-background.jpg";
 
 export const Landing = () => {
-	const [location, setLocation] = useLocation();
-	// const [cookies, setCookie, removeCookie] = useCookies([
-	// 	"token",
-	// 	"logged_in",
-	// ]);
+	const [, setLocation] = useLocation();
 
-	// // React.useEffect(() => {
-	// // 	setCookie("logged_in", cookies?.logged_in === true ? true : false);
-	// // }, []);
-
-	// const { data, error, status } = useQuery(
-	// 	["validity-data"],
-	// 	() =>
-	// 		axios.get("http://localhost:8000/api/auth/validate/", {
-	// 			headers: {
-	// 				Authorization: `Token ${cookies?.token}`,
-	// 			},
-	// 		}),
-	// 	{
-	// 		onError(err) {
-	// 			console.log(err);
-	// 			setCookie("logged_in", false, { path: "/" });
-	// 		},
-	// 		onSuccess(data) {
-	// 			setCookie("logged_in", true, { path: "/" });
-	// 		},
-	// 	}
-	// );
-	// console.log(cookies.logged_in, data);
 	return (
-		<div>
-			Landing page
-			<Button onClick={() => setLocation("/login")}>Login</Button>
-		</div>
+		<Box minH={"100vh"} minW={"100vw"} maxW={"100vw"} position={"relative"}>
+			<Stack
+				position="absolute"
+				w={"full"}
+				bgGradient={
+					"linear(to-b, blackAlpha.600, blackAlpha.500, blackAlpha.300)"
+				}
+				color="white"
+				minH={"full"}
+			>
+				<HStack justifyContent={"space-between"} py="2" px="4">
+					<HStack>
+						<Image src={viteSvg} h="20px" />
+						<Text fontFamily={"monospace"} fontWeight="semibold">
+							Budgeter
+						</Text>
+					</HStack>
+					<HStack spacing={"4"}>
+						{/* <ThemeToggler
+							aria-label="theme-toggler"
+							variant={"ghost"}
+							colorScheme={"blackAlpha"}
+							color="white"
+						/> */}
+						<Button
+							onClick={() => setLocation("/signup")}
+							variant="ghost"
+							colorScheme={"blackAlpha"}
+							color="white"
+						>
+							Sign Up
+						</Button>
+						<Button
+							onClick={() => setLocation("/login")}
+							colorScheme={"blackAlpha"}
+							color="white"
+							variant="solid"
+						>
+							Login
+						</Button>
+					</HStack>
+				</HStack>
+				<Flex
+					flexDir={"column"}
+					py="2"
+					px="4"
+					flex={1}
+					justifyContent={"center"}
+				>
+					<Stack textAlign={"center"} spacing={2}>
+						<Heading size={"3xl"} fontFamily="body">
+							Budgeting made simple.
+						</Heading>
+						<Heading
+							size={"lg"}
+							fontWeight="semibold"
+							color={"whiteAlpha.600"}
+							fontFamily="body"
+						>
+							We help manage your expenses.
+						</Heading>
+					</Stack>
+				</Flex>
+			</Stack>
+			<Image
+				src={heroBackgroundImage}
+				objectFit="cover"
+				objectPosition={"top"}
+				h="100vh"
+				// h="full"
+				w="full"
+			/>
+		</Box>
 	);
 };
