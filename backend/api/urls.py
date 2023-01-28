4ac7b733fe2e06b84cc6d64c0e2ed198da1fd52c
sarveshrane2000 <sarveshrane2000@gmail.com>
2023-01-28 20:11:15 +0530

    add: signup view

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path, include
-from knox import views as knox_views
 
 from . import views
 
 urlpatterns = [
     path("", views.HomeView.as_view()),
-    path("auth/validate/", views.ValidateToken.as_view()),
     path("auth/login/", views.LoginView.as_view()),
+    path("auth/signup/", views.SignupView.as_view()),
     path("auth/logout/", views.LogoutView.as_view()),
-    path("auth/logoutall/", knox_views.LogoutAllView.as_view(), name="knox_logoutall"),
     path("expenses/", include("expense.urls")),
     path("friends/", include("friend.urls")),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,13 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
-
-from knox.views import LoginView as KnoxLoginView
-from knox.auth import TokenAuthentication
+from rest_framework.exceptions import ParseError
 
 
 class HomeView(APIView):
@@ -15,11 +15,13 @@ class HomeView(APIView):
         return Response({"message": "Django Rest Framework - Home"})
 
 
-class LoginView(KnoxLoginView):
-    authentication_classes = [BasicAuthentication]
+class LoginView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request, format=None):
+        if not request.data["username"] or not request.data["password"]:
+            raise ParseError("Please provide both username and password.", code=400)
+
         username = request.data["username"]
         password = request.data["password"]
 
@@ -32,7 +34,7 @@ class LoginView(KnoxLoginView):
                 "loggedin", True, samesite="lax", max_age=1209600, path="/"
             )
             return response
-        return Response({"detail": "Invalid credentials."})
+        return Response({"detail": "Invalid credentials."}, status=401)
 
 
 class LogoutView(APIView):
@@ -41,15 +43,26 @@ class LogoutView(APIView):
 
     def post(self, request, format=None):
         logout(request)
-        response = Response({"detail": "Logged out."})
-        response.delete_cookie("loggedin")
+        response = Response({"detail": "User is logged out."})
+        response.delete_cookie("loggedin", path="/", samesite="lax")
         return response
 
 
-class ValidateToken(APIView):
-    authentication_classes = [TokenAuthentication, SessionAuthentication]
-    permission_classes = [IsAuthenticated]
+class SignupView(APIView):
+    permission_classes = [AllowAny]
 
-    def get(self, request, format=None):
-        content = {"detail": "Valid Token."}
-        return Response(content)
+    def post(self, request, format=None):
+        username = request.data["username"]
+        password = request.data["password"]
+        email = request.data["email"]
+
+        if not username or not password or not email:
+            raise ParseError("Please provide username, password and email.", code=400)
+
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return Response({"detail": "User already exists."}, status=400)
+
+        return Response({"detail": "User created."}, status=201)
