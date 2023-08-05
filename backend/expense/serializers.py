d90f7727f422e4dd589fba6ade8189f8ad06704c
s4rvessh04 <sarveshrane2000@gmail.com>
2023-08-06 01:31:14 +0530

    ref: exp and shared exp serializers

--- backend/expense/serializers.py
+++ backend/expense/serializers.py
@@ -7,16 +7,12 @@ from .models import Expense, SharedExpense
 from user.serializers import UserSerializer
 
 
-class SharedExpenseSerializer(serializers.ModelSerializer):
-    owner = serializers.SerializerMethodField()
+class SharedExpenseBaseSerializer(serializers.ModelSerializer):
     loaner = serializers.SerializerMethodField()
 
     class Meta:
         model = SharedExpense
         fields = "__all__"
-
-    def get_owner(self, obj):
-        return UserSerializer(obj.expense.owner).data
 
     def get_loaner(self, obj):
         return UserSerializer(obj.loaner).data
@@ -27,15 +23,29 @@ class SharedExpenseCreateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = SharedExpense
-        fields = (
-            "amount",
-            "status",
-            "loaner_id",
-        )
+        fields = ["amount", "status", "loaner_id"]
+
+
+class SharedExpenseUpdateSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    loaner_id = serializers.IntegerField()
+
+    class Meta:
+        model = SharedExpense
+        fields = ["id", "amount", "status", "loaner_id"]
+
+    # def update(self, instance, validated_data):
+    #     loaner_id = validated_data.pop("loaner_id")
+    #     loaner = User.objects.get(id=loaner_id)
+    #     instance.loaner = loaner
+    #     instance.amount = validated_data.get("amount")
+    #     instance.status = validated_data.get("status")
+    #     instance.save()
+    #     return instance
 
 
 class ExpenseListSerializer(serializers.ModelSerializer):
-    shared_expenses = SharedExpenseSerializer(many=True, read_only=True)
+    shared_expenses = SharedExpenseBaseSerializer(many=True, read_only=True)
     owner = serializers.SerializerMethodField()
 
     class Meta:
@@ -54,32 +64,46 @@ class ExpenseCreateSerializer(serializers.ModelSerializer):
         exclude = ("owner",)
 
     def create(self, validated_data):
-        is_shared = validated_data.get("is_shared", False)
         shared_expenses = validated_data.pop("shared_expenses", [])
         expense = Expense.objects.create(**validated_data)
 
-        if is_shared:
-            for shared_expense in shared_expenses:
-                try:
-                    loaner = User.objects.get(id=shared_expense.get("loaner_id"))
-                    SharedExpense.objects.create(
-                        loaner=loaner, expense=expense, **shared_expense
-                    )
-                except User.DoesNotExist:
-                    raise ValidationError(detail="User does not exist!", code=404)
+        for shared_expense in shared_expenses:
+            try:
+                loaner = User.objects.get(id=shared_expense.get("loaner_id"))
+                SharedExpense.objects.create(
+                    loaner=loaner, expense=expense, **shared_expense
+                )
+            except User.DoesNotExist:
+                raise ValidationError(detail="User does not exist!", code=404)
 
         validated_data.update({"shared_expenses": shared_expenses})
         return validated_data
 
+
+class ExpenseUpdateSerializer(serializers.ModelSerializer):
+    shared_expenses = SharedExpenseUpdateSerializer(many=True, required=False)
+
+    class Meta:
+        model = Expense
+        exclude = ["owner"]
+
     def update(self, instance, validated_data):
-        is_shared = validated_data.get("is_shared", False)
+        shared_expenses_data = validated_data.pop("shared_expenses", [])
 
-        if is_shared or validated_data.get("shared_expenses", []) != []:
-            shared_expenses_data = validated_data.get("shared_expenses", None)
-            for shared_expense in shared_expenses_data:
-                instance.shared_expenses.update(**shared_expense)
-            instance.save()
+        instance.description = validated_data.get("description", instance.description)
+        instance.amount = validated_data.get("amount", instance.amount)
+        instance.save()
 
-        Expense.objects.filter(id=instance.id).update(**validated_data)
-        instance.refresh_from_db()
+        if shared_expenses_data != []:
+            for shared_expense_data in shared_expenses_data:
+                shared_expense, created = SharedExpense.objects.update_or_create(
+                    id=shared_expense_data.get("id"),
+                    expense=instance,
+                    defaults={
+                        "loaner_id": shared_expense_data.get("loaner_id"),
+                        "amount": shared_expense_data.get("amount"),
+                        "status": shared_expense_data.get("status"),
+                    },
+                )
+
         return instance
