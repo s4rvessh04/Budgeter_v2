795e471194807cecbbf599b2615c98b32379cf59
s4rvessh04 <sarveshrane2000@gmail.com>
2023-08-11 22:35:05 +0530

    rem: unused import

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,7 +5,7 @@ from django.db import IntegrityError
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import ParseError
 
